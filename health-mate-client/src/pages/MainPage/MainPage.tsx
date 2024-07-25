e8e37d24d922a0f56199355577bbdd8896c57f1b
Vlad Pristavkin <138557453+VladPristavkin@users.noreply.github.com>
2024-07-25 15:08:03 +0300

    add main page and health images

---
+++ health-mate-client/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../moduls/Header/Header";
+import './MainPage.css';
+import RightSidebar from "../../moduls/RightSidebar/RightSidebar";
+import DashboardLayout from "../../moduls/DashboardLayout/DashboardLayout";
+
+const MainPage: React.FC = () => {
+
+    return (
+        <div className="main-page">
+            <div className="main-content">
+                
+                <div className="main-content-body">
+                   
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MainPage;
